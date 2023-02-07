62f9828f1bbc321556229591792e9db8492c0231
ZuzuStack <ridwanyamada@gmail.com>
2023-02-07 16:41:38 +0700

    first init

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Image Grid</h2>
+
+    <div>
+      <input
+        @keyup.enter="addImages"
+        ref="data"
+        type="text"
+        v-model="text"
+        id="data"
+        style="margin-bottom: 2rem"
+      />
+      <button @click="addImages" type="submit">Add</button>
+
+      <div class="grid">
+        <div
+          @click="deleteImg(index)"
+          class="border"
+          v-for="(img, index) in images"
+        >
+          <p class="text-box">Delete</p>
+          <img :src="`https://source.unsplash.com/800x800?${img}`" alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      images: [],
+      text: "",
+    };
+  },
+
+  methods: {
+    addImages() {
+      if (this.images.length < 10) {
+        this.images.push(this.text);
+        this.text = "";
+        this.$refs.data.focus();
+      }
+    },
+
+    deleteImg(index) {
+      this.images.splice(index, 1);
+    },
+  },
+
+  mounted() {
+    this.$refs.data.focus();
+  },
+};
 </script>
 
 <style>
@@ -22,5 +64,41 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.grid {
+  justify-content: center;
+  margin-inline: auto;
+  width: 100%;
+  max-width: 800px;
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.text-box {
+  cursor: pointer;
+  width: 150px;
+  transition: 0.3s;
+  display: grid;
+  place-content: center;
+  margin: 0;
+  background: #0000009b;
+  color: azure;
+  position: absolute;
+  height: 0px;
+  overflow: hidden;
+}
+
+.border:hover .text-box {
+  height: 150px;
+  width: 150px;
+}
+
+.grid img {
+  background-color: rgba(0, 0, 0, 0.318);
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
+  width: 150px;
+  height: 150px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import axios from "axios";
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+axios.defaults.baseURL = "http://localhost:3000";
+
+createApp(App).mount("#app");
